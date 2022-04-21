230a83334a0ef71c594b5faebae68b0600509b75
Alexandra Zueva <guseva-1992@yandex.ru>
2022-04-21 17:01:51 +0300

    add file 002

---
+++ 002/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход 
+// три числа и выдаёт максимальное из этих чисел.
+ 
+int a=22, b=3, c=9;
+
+if (a>b || a>c)
+{
+    Console.WriteLine( "max="+a);
+}
+else
+{
+    if (b>c)
+    {
+        System.Console.WriteLine("max=" +b);
+    }
+System.Console.WriteLine("max=" +c);
+};
